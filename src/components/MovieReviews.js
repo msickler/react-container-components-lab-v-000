5070dda73dedfe7b4734af0c86340ef8672e0fec
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 13:25:48 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+  {reviews.map(review =>
+    <div className="review">
+    {review.display_title}
+    </div>
+  )}
+  </div>
+)
+
+MovieReviews.defaultProps = {
+  reviews: ""
+}
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,21 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react'
+import 'isomorphic-fetch'
+import MovieReviews from './MovieReviews'
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: [],
+      searchTerm: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+      <MovieReviews reviews = {this.state.reviews} />
+      </div>
+    )
+  }
+}
