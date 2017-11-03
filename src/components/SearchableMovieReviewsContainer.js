3ddf8e6b84cc4320fc95b2778997fdcfe003c25e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 13:23:24 +0000

    automatically backed up by learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,3 +1,20 @@
 import React from 'react'
 import 'isomorphic-fetch'
 import MovieReviews from './MovieReviews'
+
+export default class SearchableMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      reviews: [],
+      searchTerm: ""
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+      </div>
+    )
+  }
+}
