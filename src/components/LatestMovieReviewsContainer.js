93acc35cb6e06001f8ff69cc83348c2306001c29
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 13:07:45 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -5,4 +5,25 @@ const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: []
+    }
+  }
+
+  componentWillMount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(reviews => this.setState({ reviews }))
+  }
+
+  render() {
+    return (
+      <div class="latest-movie-reviews">
+      <MovieReviews reviews={this.state.reviews}
+      </div>
+    )
+  }
+}
